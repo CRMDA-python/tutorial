b664c98ffd77b189e0371c2508b06eb61a5c38b0
j195f326 <jacob.fowles@ku.edu>
2016-11-11 10:52:17 -0600

    Create football_json.py

---
+++ week8/football_json.py
@@ -0,0 +1,55 @@
+##############################################################
+#   Playing with JSON data in Python using requests and pandas
+#   Author: Jacob Fowles
+#   Date:   11-10-16
+#   Notes:
+#   1)  Data are courtesy of www.fantasyfootballnerd.com
+##############################################################
+
+#import modules
+import os
+import pandas as pd
+import requests
+import dateutil
+
+#import JSON file into a pandas dataframe
+nfl_games = pd.read_json('data/games.json', orient='columns')
+
+#or just open it from the url directly
+url = 'http://www.fantasyfootballnerd.com/service/schedule/json/test/'
+r = requests.get(url)
+#if this worked, we will see <Response [200]>
+print(r)
+
+#create a dataframe from the request
+nfl_games = pd.DataFrame(r.json())
+
+#view the first 5 rows of the dataframe
+nfl_games.head
+
+#convert strings to separate JSON objects based on column name
+schedule = pd.read_json( (nfl_games['Schedule']).to_json(), orient='index')
+
+#look at the first few rows of the new dataframe
+schedule.head()
+
+##Do some clever things, using functions from the imported and pre-canned modules##
+
+#   how many games are there this season?
+len(schedule)
+
+#   tabulate the number of games by network
+network_counts = schedule['tvStation'].value_counts()
+network_counts
+
+#   list Denver's home schedule
+milehigh = schedule[schedule['homeTeam'] == 'DEN']
+milehigh = milehigh.sort_values(by='gameWeek')
+milehigh
+
+#   convert string gameDate to actual date
+schedule['gameDate'] = schedule['gameDate'].apply(dateutil.parser.parse, dayfirst=True)
+
+#   write the schedule dataframe to a csv file
+schedule.to_csv('data/schedule.csv', sep=',')
+
